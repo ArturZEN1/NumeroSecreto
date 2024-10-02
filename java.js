638377cf832078d6860faff6727f7aa3e826ca03
Poze <110929031+ArturZEN1@users.noreply.github.com>
2024-10-02 11:52:10 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -1,8 +1,20 @@
-alert("erro!");
+alert("adivinhe o número");
 let numeroSecreto = 9;
-let chute = prompt("que tipo de paisagem voce gosta");
+let chute;
+let tentativas =1;
+
+while(chute != numeroSecreto){
+    chute = prompt("Fale um número e veja um bom anime");
 if(chute== numeroSecreto){
 console.log("boa")
+}
+else{
+if(numeroSecreto> chute){
+alert(" numéro secreto é maior que ${chute}")
 
+}else{
+alert("numero secreto é menor que ${chute}")
 
+}
+}
 }
